fb4dc31f198a0e534338f1ab786a2aa360456c63
Andrew Shteren <38960745+2trvl@users.noreply.github.com>
2022-11-08 03:43:27 +0300

    Add video downloader

---
+++ scripts/video_downloader.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+'''
+This file is part of 2trvl/2trvl
+Personal repository with scripts and configs
+Which is released under MIT License
+Copyright (c) 2022 Andrew Shteren
+---------------------------------------------
+               Video Downloader              
+---------------------------------------------
+Downloads videos from more than a thousand
+websites. Powered by yt-dlp a youtube-dl fork
+with additional features and fixes
+
+'''
+import os
+import yt_dlp
+
+from menus import show_menu
+
+#----------------------
+#    Your Data Here    
+#----------------------
+VIDEO_URL = ""
+#  Where to download videos
+#  Leave empty to use the current directory
+DOWNLOAD_PATH = ""
+#  If there is no sound in the video
+#  Then add to it or not
+VIDEO_WITH_SOUND = True
+
+
+#------------------------
+#    Video Downloader    
+#------------------------
+ydl_opts = {
+    "quiet": True,
+    "no_color": True,
+    "format_sort": ["filesize"],
+    "ignoreerrors": "only_download"
+}
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    videoInfo = ydl.extract_info(VIDEO_URL, download=False)
+
+    sources = {}
+    
+    for source in videoInfo["formats"][::-1]:
+        if "asr" in source:
+            sourceFormatted = str(source["format_id"])
+
+            if source["asr"] is None and VIDEO_WITH_SOUND:
+                sourceFormatted += "+ba"
+
+            sources["[ {} ] [ {} ] [ {} ]".format(
+                source["format_note"],
+                source["ext"],
+                yt_dlp.utils.format_bytes(source["filesize"])
+            )] = sourceFormatted
+
+    indexes = show_menu(
+        "Choose sources to download",
+        sources.keys()
+    )
+    sources = list(sources.values())
+
+    ydl.params["format"] = ",".join([sources[index] for index in indexes])
+    ydl.format_selector = ydl.build_format_selector(ydl.params["format"])
+
+    if DOWNLOAD_PATH:
+        os.chdir(DOWNLOAD_PATH)
+
+    ydl.extract_info(VIDEO_URL)
